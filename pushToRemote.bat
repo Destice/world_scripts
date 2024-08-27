b00d6863273deb09f94a2f7bc48c36ecd2ed7649
Destice <83007545+Destice@users.noreply.github.com>
2024-08-27 23:59:48 +0200

    Update pushToRemote.bat

--- pushToRemote.bat
+++ pushToRemote.bat
@@ -1,14 +1,34 @@
+@echo off
 :RETRY_SAVE
 echo [GIT SYNC] Pushing local "world" to remote git@github.com:Destice/world.git
-(
-    cd world
-    git add .
-    git commit -m"world update"
-    git push
-    cd ..
-) || (
-    echo [GIT SYNC] Error during push!!! Retrying in 10 seconds (press Ctrl + C to cancel) - WORLD WILL NOT BE SAVED ON REMOTE!!!
-    echo [GIT SYNC] you can try to manually upload or execute "pushToRemote.bat" later
-    timeout /t 10 /nobreak > NUL
-    goto:RETRY_SAVE 
+
+cd world || (
+    echo [GIT SYNC] ERROR: Can't change dir to world!
+    goto:FAIL
 )
+git add . || (
+    cd ..
+    echo [GIT SYNC] ERROR: Can't add files to commit!
+    goto:FAIL
+)
+git commit -m"world update" || (
+    cd ..
+    echo [GIT SYNC] ERROR: Can't commit change!
+    goto:FAIL
+)
+git push || (
+    cd ..
+    echo [GIT SYNC] ERROR: Can't push change to remote
+    goto:FAIL
+)
+cd ..
+
+
+echo [GIT SYNC] Done
+timeout /t 3 /nobreak > NUL
+goto:EOF
+
+:FAIL
+echo [GIT SYNC] ERROR: World was not saved - retry in 10 seconds (press ctrl+c to cancel)...
+timeout /t 10 /nobreak > NUL
+goto:RETRY_SAVE
